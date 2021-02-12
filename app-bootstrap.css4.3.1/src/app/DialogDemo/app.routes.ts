e07eb43ec6e14a259ce3fd8b8ee159873f0da02e
shenweiliu <gliuxs2@gmail.com>
2021-02-12 16:56:36 -0500

    Delete app-bootstrap.css4.3.1/src/app/DialogDemo directory

--- app-bootstrap.css4.3.1/src/app/DialogDemo/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { SampleMainComponent } from './sample-main.component';
-import { SampleSecondComponent } from './sample-second.component';
-
-export const routes: Routes = [
-    { path: "", redirectTo: "main", pathMatch: "full" },    
-    { path: 'main', component: SampleMainComponent },
-    { path: 'second', component: SampleSecondComponent }
-];
