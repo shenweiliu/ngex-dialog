ec564ce3ffe3f2fef05f5b49397ff6552cafa889
shenweiliu <gliuxs2@gmail.com>
2025-01-19 12:38:09 -0500

    Add files via upload

---
+++ lib-build/projects/ngex-dialog/src/lib/dialog.module.ts
@@ -0,0 +1,7 @@
+export { DialogComponent } from './dialog.component';
+export { BasicDialogComponent } from './basic-dialog.component';
+export { DialogService } from './dialog.service';
+export { ExDialog } from './ex-dialog.service';
+export { FocusBlurDirective } from './focus-blur.directive';
+export { NgExDialogConfig } from './dialog-config';
+export { DialogCache } from './dialog-cache';
---
+++ lib-build/projects/ngex-dialog/src/public-api.ts
@@ -0,0 +1,12 @@
+/*
+ * Public API Surface of ngex-dialog
+ */
+
+export * from "./lib/dialog-host.component";
+export * from './lib/basic-dialog.component';
+export * from './lib/focus-blur.directive';
+export * from "./lib/dialog-config";
+export * from "./lib/dialog.service";
+export * from "./lib/ex-dialog.service";
+export * from "./lib/dialog-cache";
+export * from './lib/dialog.module';
