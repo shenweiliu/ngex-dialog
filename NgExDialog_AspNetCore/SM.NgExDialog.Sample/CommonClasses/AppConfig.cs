526d090e705a001c4ec753a368366d83b1cff10c
shenweiliu <gliuxs2@gmail.com>
2025-01-19 17:51:11 -0500

    Add files via upload

---
+++ NgExDialog_AspNetCore/SM.NgExDialog.Sample/CommonClasses/AppConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SW.NgTestCore.Web
+{
+    public class AppConfig
+    {
+        public string TestConfig1 { get; set; }
+        public bool UseInMemoryDatabase { get; set; }
+    }
+}
